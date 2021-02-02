cf563d248a07479eed47bf2866af8a2153a62bea
MrAisenodni <fikrialfayed@gmail.com>
2021-02-02 19:32:13 +0700

    fixed form

--- admin/edit-imunisasi.php
+++ admin/edit-imunisasi.php
@@ -47,6 +47,7 @@ if(isset($_POST['tambah'])){
                                         <label for="exampleInputPassword1" class="form-label">Nama Balita</label>
                                         <input type="text" name="" class="form-control" id="" readonly value="<?= $data2['nama_balita']." | ".$data2['nama'] ?>">
                                     </div><br>
+                                    <p><b style="color: red">Note:</b><br><br>* Jarak antara (interval) pemberian vaksin DPT-HB-Hib minimal 4 minggu (1 bulan). Jarak antara pemberian vaksin Polio minimal 4 minggu (1 bulan)<br>** Anak diatas umur 1 tahun (12 bulan) yang belum lengkap imunisasinya tetap harus diberikan imunisasi dasar lengkap. Sakit ringan seperti batuk, pilek, diare, demam ringan dan sakit kulit bukan halangan untuk imunisasi<br></p>
                             </div>
                             <div class="col-sm-6"><br><br>
                                     <div class="sm-3">
@@ -71,7 +72,7 @@ if(isset($_POST['tambah'])){
                                     <div class="sm-3">
                                         <label for="exampleInputPassword1" class="form-label">Tanggal Imunisasi</label>
                                         <input type="date" name="tgl" class="form-control" id="" value="<?= $data['tgl_imunisasi'] ?>">
-                                    </div><br>
+                                    </div><br><br><br><p>*** Pemberian DPT-HB-Hib lanjutan diberikan minimal 12 bulan setelah pemberian imunisasi DPT-HB-Hib 3 dan dapat diberikan dalam renatng usia 18-24 bulan<br>**** Pemberian imunisasi campak lanjutan diberikan minimal 6 bulan setelah pemberian imunisasi campak terakhir dan dapat diberikan dalam rentang usia 18-24 bulan</p>
                                     <hr>
                                     <button name="tambah" class="btn btn-success btn-lg"><i class="fa fa-check-square"></i> Simpan</button>
                                     <a href="imunisasi.php" class="btn btn-danger btn-lg"><i class="fa fa-times"></i> Batal</a>
